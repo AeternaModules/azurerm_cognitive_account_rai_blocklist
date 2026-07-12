411badb6305f8bb0f40cee8d9ea66247d854103b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "cognitive_account_rai_blocklists" {
-  description = "All cognitive_account_rai_blocklist resources"
-  value       = azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists
-}
 output "cognitive_account_rai_blocklists_cognitive_account_id" {
-  description = "List of cognitive_account_id values across all cognitive_account_rai_blocklists"
-  value       = [for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : v.cognitive_account_id]
+  description = "Map of cognitive_account_id values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
+  value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.cognitive_account_id }
 }
 output "cognitive_account_rai_blocklists_description" {
-  description = "List of description values across all cognitive_account_rai_blocklists"
-  value       = [for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : v.description]
+  description = "Map of description values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
+  value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.description }
 }
 output "cognitive_account_rai_blocklists_name" {
-  description = "List of name values across all cognitive_account_rai_blocklists"
-  value       = [for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : v.name]
+  description = "Map of name values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
+  value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.name }
 }
 output "cognitive_account_rai_blocklists_tags" {
-  description = "List of tags values across all cognitive_account_rai_blocklists"
-  value       = [for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : v.tags]
+  description = "Map of tags values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
+  value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.tags }
 }
 
