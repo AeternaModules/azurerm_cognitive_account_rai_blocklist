be105e5130ea111b27e73ba1acd779db4e4a527e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cognitive_account_rai_blocklists_id" {
+  description = "Map of id values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
+  value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.id }
+}
 output "cognitive_account_rai_blocklists_cognitive_account_id" {
   description = "Map of cognitive_account_id values across all cognitive_account_rai_blocklists, keyed the same as var.cognitive_account_rai_blocklists"
   value       = { for k, v in azurerm_cognitive_account_rai_blocklist.cognitive_account_rai_blocklists : k => v.cognitive_account_id }
